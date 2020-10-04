a372e5f70e076952e58451b3fe1e65813d10fe60
junise1 <kevin.julian.torres117@gmail.com>
2020-10-03 20:08:31 -0500

    mensajito

--- Controllers/Usuario.cs
+++ Controllers/Usuario.cs
@@ -7,16 +7,22 @@ using System.Web.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Adopciones.Controllers
+namespace Adopciones1.Controllers
 {
     [Route("api/[controller]")]
-    public class Prueba : Controller
+    public class Usuario : Controller
     {
         // GET: api/<controller>
         [HttpGet]
         public string getUsuario()
         {
             return "Pablito bosudo";
+        }
+
+        public int traerSuma(int valor1, int valor2)
+        {
+            int resultado = valor1 + valor2;
+            return resultado;
         }
 
     }
